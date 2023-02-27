b3f2b49ecef95c721ed7277fab612919d73adf71
jeogooussama <jeogodz@protonmail.com>
2023-02-27 21:44:47 +0100

    new file:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   server.js 	new file:   uploads/bb7db35e-df2b-4632-acc0-9645990b524d.webm 	new file:   uploads/recording.webm

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 // Set up multer to handle file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => { 
-    
     cb(null, './uploads'); // Save the uploaded file to the 'uploads' directory
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname); // Use the original file name
+    const uniqueName = uuidv4(); // Generate a unique name for the video file
+    const fileExtension = path.extname(file.originalname);
+    cb(null, `${uniqueName}${fileExtension}`); // Use the generated unique name for the file
   }
 });
 const upload = multer({ storage });
@@ -27,10 +29,21 @@ app.post('/start-recording', (req, res) => {
 
 // Handle the POST request to stop recording and upload the recorded video
 app.post('/stop-recording', upload.single('video'), (req, res) => {
+  if (!req.file) {
+    res.status(400).json({ error: 'No file was uploaded' });
+    return;
+  }
+
   const filePath = req.file.path; // Get the file path of the uploaded video
   // Save the file path to the database or do other logic here
 
   res.json({ filePath });
+});
+
+// Handle errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong' });
 });
 
 // Start the server
